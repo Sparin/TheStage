d4879937d6dfa0bb66292341a50a7046e15e45fa
Sparin <sparin285@gmail.com>
2016-07-28 18:48:08 +0300

    Чистка кода и задачи в виде TODO

--- TheStage/TheStage/ViewModel/wndGameControllerViewModel.cs
+++ TheStage/TheStage/ViewModel/wndGameControllerViewModel.cs
@@ -75,7 +75,8 @@ namespace TheStage.ViewModel
         public Command GestureClickedCommand { get; private set; }
 
 
-        //private List<>
+        //Legacy
+        //TODO: Rework click handler
         TextBlock basicStatus;
 
         public wndGameControllerViewModel(string levelDirectory)
@@ -90,119 +91,33 @@ namespace TheStage.ViewModel
             if (!File.Exists(levelDirectory + "/map.csv"))
                 throw new FileNotFoundException("map.csv not found");
 
-            mediaPlayer.Source = new Uri(videoPath, UriKind.Absolute);
-            mediaPlayer.LoadedBehavior = MediaState.Manual;
-            mediaPlayer.Stretch = Stretch.UniformToFill;
-
-            GameObjects.Add(mediaPlayer);
-
             //TODO: Get Dimensions of media file
             Width = 1280;
             Height = 720;
 
-            //Legacy too
+            mediaPlayer.Source = new Uri(videoPath, UriKind.Absolute);
+            mediaPlayer.LoadedBehavior = MediaState.Manual;
+            mediaPlayer.Stretch = Stretch.UniformToFill;
+            mediaPlayer.Width = width;
+            mediaPlayer.Height = height;
+
+            GameObjects.Add(mediaPlayer);            
+
+            //Legacy
+            //TODO: Rework
             GestureClickedCommand = new Command(GestureClicked);
 
             ReadMap(levelDirectory + "/map.csv");
 
+            //TODO: Synchronization logic            
             mediaPlayer.Play();
             for (int i = 0; i < primitives.Count; i++)
                 primitives[i].BeginStoryboard(animations[i]);
-
-
-            #region Sample
-            //MediaElement me = new MediaElement();
-            //me.Source = new Uri(@"C:\Users\spari\Desktop\14692643770240.webm", UriKind.Absolute);
-            //me.LoadedBehavior = MediaState.Manual;
-            //me.Stretch = Stretch.UniformToFill;
-            //me.Width = Width;
-            //me.Height = Height;
-            //me.Loaded += (e, s) =>
-            //{
-            //    me.Play();
-            //};
-
-            //Path basicSquare = new Path();
-            //basicSquare.Stroke = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-            //basicSquare.Data = (Geometry)basicSquare.FindResource("Square");
-
-            //Path basicPlaceholder = new Path();
-            //basicPlaceholder.Stroke = new SolidColorBrush(Color.FromRgb(0, 255, 0));
-            //basicPlaceholder.Fill = basicPlaceholder.Stroke;
-            //basicPlaceholder.Data = (Geometry)basicSquare.FindResource("Square");
-
-            //Canvas.SetLeft(basicSquare, -100);
-            //Canvas.SetTop(basicSquare, -100);
-
-            //Canvas.SetLeft(basicPlaceholder, 1000);
-            //Canvas.SetTop(basicPlaceholder, 500);
-
-            //PathGeometry way = new PathGeometry();
-            //way.AddGeometry(Geometry.Parse("M -100 -100 C-100 -100 100 500 1000 500"));
-
-            #region Animation Sample
-            //Storyboard basicAnimation = new Storyboard();
-
-            //DoubleAnimationUsingPath dx = new DoubleAnimationUsingPath();
-            //dx.PathGeometry = way;
-            //dx.BeginTime = TimeSpan.FromSeconds(2);
-            //dx.Duration = TimeSpan.FromSeconds(4);
-            //dx.Source = PathAnimationSource.X;
-            //Storyboard.SetTargetProperty(dx, new PropertyPath(Canvas.LeftProperty));
-
-            //DoubleAnimationUsingPath dy = new DoubleAnimationUsingPath();
-            //dy.PathGeometry = way;
-            //dy.BeginTime = TimeSpan.FromSeconds(2);
-            //dy.Duration = TimeSpan.FromSeconds(4);
-            //dy.Source = PathAnimationSource.Y;
-            //Storyboard.SetTargetProperty(dy, new PropertyPath(Canvas.TopProperty));
-
-            //basicAnimation.Children.Add(dx);
-            //basicAnimation.Children.Add(dy);
-            #endregion
-
-            //basicAnimation.Completed += (s, e) =>
-            //{
-            //    GameObjects.Remove(basicPlaceholder);
-            //    GameObjects.Remove(basicSquare);
-            //    GameObjects.Add(basicStatus);
-            //};
-
-
-            //basicStatus = new TextBlock();
-            //basicStatus.Text = "Bad";
-            //basicStatus.FontSize = 24;
-            //basicStatus.FontFamily = new FontFamily("Impact");
-            //basicStatus.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-
-            //Canvas.SetLeft(basicStatus, 1025);
-            //Canvas.SetTop(basicStatus, 500);
-
-            //Storyboard statusStory = new Storyboard();
-            //DoubleAnimation basicAnimationStatusOpacity = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(1));
-            //DoubleAnimation basicAnimationStatusTop = new DoubleAnimation(500, 400, TimeSpan.FromSeconds(1));
-            //Storyboard.SetTargetProperty(basicAnimationStatusTop, new PropertyPath(Canvas.TopProperty));
-            //Storyboard.SetTargetProperty(basicAnimationStatusOpacity, new PropertyPath(Control.OpacityProperty));
-
-            //statusStory.Children.Add(basicAnimationStatusTop);
-            //statusStory.Children.Add(basicAnimationStatusOpacity);
-            //statusStory.Completed += (s, e) => GameObjects.Remove(basicStatus);
-            //basicStatus.Loaded += (s, e) =>
-            //{
-            //    basicStatus.BeginStoryboard(statusStory);
-            //    //basicStatus.BeginAnimation(Control.OpacityProperty, basicAnimationStatusOpacity);
-            //    //basicStatus.BeginAnimation(Canvas.TopProperty, basicAnimationStatusTop);
-            //};
-
-
-            //basicSquare.Loaded += (s, e) => basicSquare.BeginStoryboard(basicAnimation);
-            //GameObjects.Add(me);
-            //GameObjects.Add(basicPlaceholder);
-            //GameObjects.Add(basicSquare);
-            #endregion
         }
 
         //TODO: Create styles for the game objects
+        //TODO: Make a new class with placeholder, primitive, animations and status references 
+        //      with default values for each object
         private void ReadMap(string path)
         {
             string[] objects = File.ReadAllLines(path);
