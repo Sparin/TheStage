7bcd5afc5c2a764f491fdebca0e7c5f2c49df778
Medveditskov Yuriy Sergeevich <sparin285@gmail.com>
2016-08-31 19:14:11 +0300

    Демо коммит

--- TheStage/TheStage/ViewModel/wndMenuViewModel.cs
+++ TheStage/TheStage/ViewModel/wndMenuViewModel.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace TheStage.ViewModel
 {
@@ -20,9 +21,39 @@ namespace TheStage.ViewModel
 
         public wndMenuViewModel()
         {
+            Application.Current.Exit += (s, e) => { MessageBox.Show(
+@"The Stage - Demo (build #870da92) версия для конкурса ВОКИ
+---------------------------------------------------------------------
+
+Sparin - vk.com/sparin - Программист костылей и автор карты
+Rougeasus - vk.com/id106856544 - Дизайнер в разных стилях
+
+OhPonyBoy - facebook.com/OhPonyBoy - Автор The Blob Symphony
+
+--------------------------------------------------------------------
+
+Официальная страница проекта: github.com/Sparin/TheStage
+Лицензия: MIT", "Авторы"); };
+
+            MessageBox.Show(
+@"Краткая инструкция, перед началом демо уровня
+W или I - треугольник
+A или J - квадрат
+S или K - крест
+D или L - круг
+
+Фигура - одиночное нажатие
+Стрелка - двойное нажатие
+Фигура в круге - зажатие клавиши на время, указанное под кругом
+
+
+Подробно иллюстрированную инструкцию вы можете найти на GitHub'е проекта или странице конкурса ВОКИ","How To Play");
             wndGameController controller = new wndGameController();
             controller.ContentRendered += (s, e) => { controller.DataContext = new wndGameControllerViewModel(AppDomain.CurrentDomain.BaseDirectory + @"Resources\Missions\[Demo] The Blob Symphony");};
             controller.ShowDialog();
+            if (Application.Current != null)
+                Application.Current.Shutdown();
+           
             //new wndGameController() { DataContext = new wndGameControllerViewModel(AppDomain.CurrentDomain.BaseDirectory + @"Resources\Missions\[Demo] The Blob Symphony") }.ShowDialog();
         }
 
